eef446cd34c4da6a849b0a6f4eb656bbb99dbe5b
Pouki8 <severine.zapata@orange.fr>
2025-01-05 18:29:48 +0100

    fond d'écran sur page Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FondEcranComponent } from './composant/fond-ecran/fond-ecran.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FondEcranComponent
+    FondEcranComponent,
+    PageHomeComponent
   ],
   imports: [
     BrowserModule,
